67a70fe042074f0032f213528af475f16f205769
Yabel90 <yasminabelhaj@hotmail.com>
2024-11-13 00:37:43 +0000

    chore todo list

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,24 +1,45 @@
-import React from "react";
+import React, { useState } from "react";
 
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
+import { ImCross } from "react-icons/im";
 
 //create your first component
 const Home = () => {
+	const [tool, setTool] = useState('');
+	const [toolList, setToolList] = useState([]);
+
+	const handleSubmit = e => {
+		e.preventDefault();
+		if (tool.trim()) {
+			setToolList([...toolList, { label: tool }]);
+			setTool("");
+		}
+	};
+
+	const handleChange = e => setTool(e.target.value)
+
+	const handleDelete = (index) => {
+		setToolList(toolList.filter((_, i) => i !== index))
+	}
 	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+		<div className="container">
+			<div className="flower-headding">
+				<h1 className="tools" >TOOLS</h1>
+				<img className="flower" src="https://img.freepik.com/fotos-premium/primer-plano-flores-rosadas-contra-fondo-blanco_1048944-20536012.jpg?w=1380" alt="flores" />
+
+				<div className="wrapper">
+					<form onSubmit={handleSubmit}>
+						<input className="placetool" type="text" value={tool} onChange={handleChange} placeholder="Add a new tool" />
+					</form>
+					<ul>
+						{toolList.length > 0 ? (
+							toolList.map((tool, i) => (<li className="text-item" key={i}>{tool.label} <span className="imcross" onClick={() => handleDelete(i)}><ImCross /></span> </li>))
+						)
+							: (<h5 className="h5">Add some Tools</h5>)}
+					</ul>
+					<p className="item"> {toolList.length} {toolList.length === 1 ? "item" : "items"} </p>
+				</div>
+
+			</div>
 		</div>
 	);
 };
